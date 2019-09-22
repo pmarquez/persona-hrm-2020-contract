0d69dc5fc031bb63e5ebe92215faa4a6f5c1ba55
Paulo Márquez Herrero <me@pmarquezh.info>
2019-09-22 15:11:36 -0700

    First commit into independent Persona-HRM-2020 Contract microservice.

--- src/main/java/io/nordstar/personahrm/contract/ContractApplication.java
+++ src/main/java/io/nordstar/personahrm/contract/ContractApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ContractApplication {
 
+    /**
+     *
+     * @param args
+     */
     public static void main ( String[] args ) {
         SpringApplication.run ( ContractApplication.class, args );
     }
