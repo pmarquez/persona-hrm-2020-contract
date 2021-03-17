b019a40eaecb1e8db5b8e0d6efe19f69f03169c9
Paulo Márquez Herrero <me@pmarquezh.info>
2021-03-17 19:21:42 +0100

    ContractDAO II.

--- src/main/java/io/nordstar/personahrm/contract/dao/ContractDAO.java
+++ src/main/java/io/nordstar/personahrm/contract/dao/ContractDAO.java
@@ -1,29 +1,24 @@
 package io.nordstar.personahrm.contract.dao;
 
 //   Standard Libraries Imports
-
+import java.util.ArrayList;
+import java.util.List;
 
 //   Third Party Libraries Imports
-
-
-//   FENIX Framework Imports
-
-
-//   Application Domain Imports
-
-
-import io.nordstar.personahrm.contract.model.contract.ContractBaseRec;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.InvalidResultSetAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
+//   FENIX Framework Imports
+
+
+//   Application Domain Imports
+import io.nordstar.personahrm.contract.model.contract.ContractBaseRec;
 
 /**
- * ContractRepositoryImpl.java<br/><br/>
+ * ContractDAOImpl.java<br/><br/>
  * Creation Date 2019-10-07 15:18 PT<br/><br/>
  * <b>DESCRIPTION:</b><br/><br/>
  * <p></p>
@@ -43,7 +38,7 @@ import java.util.List;
  * @author Paulo Márquez
  * @version 1.0 - 2019-10-07 15:18 PT
  */
-public class ContractRepositoryImpl implements ContractRepository {
+public class ContractDAO {
     @Autowired
     private JdbcTemplate contractJdbcTemplate;
 
@@ -51,19 +46,19 @@ public class ContractRepositoryImpl implements ContractRepository {
     private NamedParameterJdbcTemplate contractNpJdbcTemplate;
 
     //   Contracts CRUD
-    private static final String RETRIEVE_CONTRACTS_SQL_QUERY =   "SELECT hrm_ctrc_contractentity.contractCode, "        +
-                                                                        "hrm_ctrc_contractentity.contractId, "          +
-                                                                        "hrm_ctrc_contractentity.contractTypeCode, "    +
-                                                                        "hrm_ctrc_contractentity.personCode, "          +
-                                                                        "hrm_ctrc_contractentity.companyCode, "         +
-                                                                        "hrm_ctrc_contractentity.creationDate, "        +
-                                                                        "hrm_ctrc_contractentity.activationDate, "      +
-                                                                        "hrm_ctrc_contractentity.terminationDate, "     +
-                                                                        "hrm_ctrc_contractentity.active "               +
+    private static final String RETRIEVE_CONTRACTS_SQL_QUERY =  "SELECT hrm_ctrc_contractentity.contractCode, "        +
+                                                                       "hrm_ctrc_contractentity.contractId, "          +
+                                                                       "hrm_ctrc_contractentity.contractTypeCode, "    +
+                                                                       "hrm_ctrc_contractentity.personCode, "          +
+                                                                       "hrm_ctrc_contractentity.companyCode, "         +
+                                                                       "hrm_ctrc_contractentity.creationDate, "        +
+                                                                       "hrm_ctrc_contractentity.activationDate, "      +
+                                                                       "hrm_ctrc_contractentity.terminationDate, "     +
+                                                                       "hrm_ctrc_contractentity.active "               +
 
-                                                                 "FROM hrm_ctrc_contractentity "                        +
+                                                                "FROM hrm_ctrc_contractentity "                        +
 
-                                                                 "ORDER BY ACTIVATION_DATE";
+                                                                "ORDER BY ACTIVATION_DATE";
 
     private static final String RETRIEVE_CONTRACT_BY_CODE_SQL_QUERY =   "SELECT hrm_ctrc_contractentity.contractCode, "        +
                                                                                "hrm_ctrc_contractentity.contractId, "          +
@@ -79,12 +74,10 @@ public class ContractRepositoryImpl implements ContractRepository {
 
                                                                         "WHERE hrm_ctrc_contractentity.contractCode = ";
 
-
     /**
      *
      * @return
      */
-    @Override
     public List<ContractBaseRec> retrieveContracts ( ) {
 
         List<ContractBaseRec> persons = new ArrayList<> ( );
@@ -92,17 +85,17 @@ public class ContractRepositoryImpl implements ContractRepository {
         try {
             persons = contractJdbcTemplate.query ( RETRIEVE_CONTRACTS_SQL_QUERY,
                     ( rs, rowNum ) -> new ContractBaseRec (   rs.getInt     ( "contractCode"     ),
-                                                              rs.getString  ( "contractId"       ),
-                                                              rs.getInt     ( "contractTypeCode" ),
-                                                              "",
-                                                              rs.getInt     ( "personCode"       ),
-                                                              null,
-                                                              rs.getInt     ( "companyCode"      ),
-                                                              null,
-                                                              rs.getString  ( "creationDate"     ),
-                                                              rs.getString  ( "startDate"        ),
-                                                              rs.getString  ( "terminationDate"  ),
-                                                              rs.getBoolean ( "active"           )
+                            rs.getString  ( "contractId"       ),
+                            rs.getInt     ( "contractTypeCode" ),
+                            "",
+                            rs.getInt     ( "personCode"       ),
+                            null,
+                            rs.getInt     ( "companyCode"      ),
+                            null,
+                            rs.getString  ( "creationDate"     ),
+                            rs.getString  ( "startDate"        ),
+                            rs.getString  ( "terminationDate"  ),
+                            rs.getBoolean ( "active"           )
                     )
             );
 
